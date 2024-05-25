b5a5fe65b06efed0e93ea85cd66098faa77cb6eb
NML124 <nehemielwatumba@gmail.com>
2024-05-25 19:42:03 +0100

    Add strings constants

---
+++ lib/Constants/strings.dart
@@ -0,0 +1,66 @@
+class StringsConstant {
+  static const String nameApp = "Evodie";
+  static const String sommeactuelleConst = "Somme actuelle";
+  static const String proprietaire = "Propriétaire";
+  static const String vendeur = "Vendeur";
+  static const String vendeuse = "Vendeuse";
+
+  static const String journalier = "Journalier";
+  static const String hebdomadaire = "hebdomadaire";
+  static const String mensuel = "Mensuel";
+  static const String Annuel = "Annuel";
+
+  static const String benefice = "Bénéfice";
+  static const String perte = "Perte";
+  static const String dette = "Dette";
+
+  static const String stock = "Stock";
+
+  static const String debiteur = "Débiteur";
+  static const String paiementDette = "Paiement dette";
+  static const String sommeActuellePayer = "Somme actuelle à payer";
+  static const String paiementDe = "Paiement de";
+  static const String sommePayee = "Somme payée";
+
+  static const String produit = "Produit";
+  static const String quantite = "Quantité";
+
+  static const String nom = "Nom";
+  static const String prenom = "Prénom";
+
+  static const String nomDebiteur = "Nom du débiteur";
+  static const String prenomDebiteur = "Prénom du débiteur";
+
+  static const String nomUtilisateur = "Nom utilisateur";
+  static const String prenomUtilisateur = "Prénom utilisateur";
+
+  static const String produitVendu = "produit vendu";
+  static const String quantiteVendu = "Quantité vendue";
+
+  static const String produitCommande= "produit commandé";
+  static const String prixCommande = "Prix de la commande";
+  static const String quantiteCommande = "Quantité de paquet commandé";
+
+  static const String produitLivre = "Produit livré";
+  static const String quantiteLivre = "Quantité de paquet livré";
+
+  static const String bouteilles = "Bouteilles";
+  static const String paquets = "Paquets";
+
+  static const String commande = "Commande";
+  static const String livraison = "Livraison";
+
+  static const String profile = "Profile";
+
+  static const String ancienMotDePasse = "Ancien mot de passe";
+  static const String nouveauMotDePasse = "nouveau mot de passe";
+
+  static const String enregistrer = "Enregistrer";
+  static const String seDeconnecter = "Se Deconnecter";
+  
+  static const String annuler = "Annuler";
+  static const String payer = "Payer";
+
+  
+
+}
