57226647c867a5ff8b47d7d95c60a3d74b8d2294
NML124 <nehemielwatumba@gmail.com>
2024-05-25 17:46:20 +0100

    Colors constants

---
+++ lib/Constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorsConstant {
+  static const Color green = Color.fromARGB(255, 44, 165, 141);
+  static const Color red = Color.fromARGB(255, 232, 72, 85);
+  static const Color white = Color.fromARGB(255, 255, 253, 247);
+  static const Color black = Color.fromARGB(255, 10, 35, 66);
+  static const Color gray = Color.fromARGB(255, 136, 132, 132);
+  static const Color purple = Color.fromARGB(255, 167, 129, 216);
+}
